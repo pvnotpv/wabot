d19bb4b2d2491ff21ad43578a8722e8933e887f7
dumbusername <66077878+pvnotpv@users.noreply.github.com>
2023-03-05 16:25:09 +0530

    Update about.js

--- Commands/Misc/about.js
+++ Commands/Misc/about.js
@@ -1,5 +1,5 @@
 const execute = async (client, msg) => {
-    await msg.reply('*VERSION* -> 3.7.0 \nSource Code -> The bot is open source.')
+    await msg.reply('*VERSION* -> 3.7.0 \nSource Code -> https://github.com/pvnotpv/wabot')
     return;
 }
 
